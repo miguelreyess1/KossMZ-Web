aa91f15be701696fde0fc4e4aed51d63e5933e99
miguelreyess1 <miguel.reyesgomez1@gmail.com>
2025-04-08 11:25:26 +0200

    Mejoras

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -2,7 +2,7 @@ export default function Cabecera() {
   return (
     <header className="cabecera">
       <h1 className="nombre-artista">KOSS MZ</h1>
-      <h2 className="subtitulo">Próximo lanzamiento</h2>
+      <h2 className="subtitulo">Piso 9</h2>
     </header>
   );
 }
