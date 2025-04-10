1fad1a80f6a25c53aeacf7ff709f19b08d1a67e2
miguelreyess1 <miguel.reyesgomez1@gmail.com>
2025-04-10 09:31:37 +0200

    Mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// App.jsx
 import React from 'react';
 import Cabecera from './components/Cabecera';
 import ContadorRegresivo from './components/ContadorRegresivo';
 import Escena from './components/EsferasRedes';
 import FondoEstelar from './components/Background/FondoEstelar';
+import { Analytics } from '@vercel/analytics/react';
 import './styles/global.css';
 
 export default function App() {
@@ -12,9 +12,10 @@ export default function App() {
       <FondoEstelar />
       <Cabecera />
       <main className="contenido-principal">
-        <ContadorRegresivo fechaLanzamiento="2025-08-01T00:00:00" />
-          <Escena />  
+        <ContadorRegresivo fechaLanzamiento="2025-04-18T00:00:00" />
+        <Escena />  
       </main>
+      <Analytics />
     </div>
   );
 }
